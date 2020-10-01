6a136b0d7ed38b39ca53b903739aeb5411d35a40
Axel3246 <hplaxel@hotmail.com>
2020-10-01 12:14:49 -0500

    Dictionary and Hash Tables
    First Look at Hash Tables and Dictionaries. YT channels can be found at line 1 of code.

--- DS Practice/5.0.py
+++ DS Practice/5.0.py
@@ -1,12 +1,18 @@
 #https://www.youtube.com/watch?v=daefaLgNkw0&ab_channel=CoreySchafer
 Estudiante = {'nombre': 'Jhon','edad': 25, 'cursos':['Matemáticas', 'Computación']} #Empieza el dict
 
+#o
+
+Estudiante1 = dict(Nombre ='Jhon', Edad = 25) #También se puede de esta forma
+
+
 Estudiante['telefono'] = '555-555-555' #Agrega una llave con un valor al diccionario
 
-Estudiante.update({'nombre':'Axel', 'edad': 18}) #Modifica el diccionario con ek .update()
+Estudiante.update({'nombre':'Axel', 'edad': 18}) #Modifica el diccionario con el .update()
 del Estudiante['edad'] #Elimina una llave del diccionario
 
 print(Estudiante) #Imprime el dict
+print(Estudiante1)
 print(Estudiante.get('nombre')) #Imprime solo el nombre o cualquier otra variable en el dict
 print(Estudiante.get('telefono', 'No se encontró')) #Si no existe la llave, retorna el mensaje que no lo encontró
 
@@ -17,5 +23,5 @@ print(Estudiante.items()) #Devuelve ambas cosas
  
 #iteration
 
-for i in Estudiante.items():
+for i in Estudiante.items():   #o .values, o.keys, o simplemente Estudiante
     print(i)
---
+++ DS Practice/5.1.py
@@ -0,0 +1,11 @@
+#https://www.youtube.com/watch?v=APAbRkrqDVI&ab_channel=edureka%21
+#Nested Dictionaries
+
+nestesdict = {'Alumno': {'Axel':{'Edad': 18, 'Materias': 'Matemáticas', 'Puesto': 'Estudiante'},
+            'Rem':{'Edad': 18, 'Materias': 'Física', 'Puesto': 'Estudiante'}}}
+print(nestesdict)
+
+#Access
+
+nestesdict['Alumno']
+
