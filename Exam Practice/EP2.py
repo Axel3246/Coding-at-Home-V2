83ffa46a4b8344425539e604f26946a82f0e92ab
Axel3246 <hplaxel@hotmail.com>
2020-10-19 20:06:58 -0500

    Conditionals
    Use of if/elif/else conditionals

---
+++ Exam Practice/EP2.py
@@ -0,0 +1,22 @@
+'''
+Conditionals
+'''
+
+x = 6
+y = 4
+
+if x > 2:
+    print (x)
+    if y < 4:
+        print (y)
+    else:
+        print (x + y)        #R: 6 y 10
+
+w = 1
+z = 2
+if w > 5:
+    print (x)
+elif z < 5:
+    print (z)
+else:
+    print(x + y)   #R: 2
