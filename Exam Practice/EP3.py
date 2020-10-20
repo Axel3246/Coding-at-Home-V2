1f85ccef26392b469ed4e5aaed3f704414628a22
Axel3246 <hplaxel@hotmail.com>
2020-10-20 07:50:38 -0500

    Funciones
    Funtion excercises for the exam.

---
+++ Exam Practice/EP3.py
@@ -0,0 +1,29 @@
+'''
+Funciones
+'''
+'''
+def calculo(x,y,z):
+    result = x + y * z
+    return result
+
+def main():
+    p = 1
+    q = 2
+    r = 3
+    s = calculo(p,q,r)
+    print(s)   #R: 7
+
+main() 
+'''
+
+def tres(x, y):
+    x = 3
+    y = 6
+
+def main() :
+    p = 10
+    q = 20
+    tres(p,q)
+    print(p, q)
+
+main()
