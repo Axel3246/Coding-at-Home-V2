77e5daca9b353744d6406b82aad42e9742f5796f
Axel3246 <hplaxel@hotmail.com>
2020-10-19 20:06:37 -0500

    Basic Operations
    Use of basic operations and booleans

---
+++ Exam Practice/EP1.py
@@ -0,0 +1,10 @@
+'''
+Basic Operations
+'''
+print((5 + 11 // 3) / (2 ** 3) + 2) # R: 3
+
+print((not (5 != 8) or (3.0 >= 3))) # True
+
+print(3.0 == 3) # True
+
+print(3.0 < 3) # False for any case
