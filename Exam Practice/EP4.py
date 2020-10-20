79166508687631eb5d1cfb5c70e83202f3d82607
Axel3246 <hplaxel@hotmail.com>
2020-10-20 07:50:54 -0500

    Loops
    Some Loop Excercises for the exam.

---
+++ Exam Practice/EP4.py
@@ -0,0 +1,31 @@
+'''
+Loops
+'''
+for b in range (10,1,-2):
+    print(b) #R: 10, 8 ,6 ,4 ,2
+
+for e in range(0,60,11):
+    print(e) #R: 0, 11, 22, 33, 44, 55
+
+y = 12
+z = 2
+while y > z:
+    print(y,z) # 12, 2 ; 10 , 3 ; 7 , 4
+    y = y - z # 12, 10 , 7
+    z = z + 1 # 2, 3 , 4
+
+
+valor = 8
+
+while valor > 10:
+      valor = valor - 1
+
+print (valor)
+
+for cont in range(3, 8, 2):
+
+    print(cont)
+
+for cont in range(6):
+
+    print(cont)
