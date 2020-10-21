a8262058a3aab6e02592b487587900d425cbdc7b
Axel3246 <hplaxel@hotmail.com>
2020-10-21 18:11:46 -0500

    Strings
    Some strings excercises

---
+++ Exam Practice/EP5.py
@@ -0,0 +1,17 @@
+'''
+Strings
+'''
+
+str = "Computación"
+
+print(str[-6:-3]) # tac
+
+print(str[-3:]) # ion
+
+print(str[0:5]) # Compu
+
+print(str[3:8]) # putac
+
+
+b = a = 5
+print ('a' != 'A')
